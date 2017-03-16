2c37c3b284cd337da3aa6c044e4d88bb12bb07b2
joncoop <joncoop1972@gmail.com>
2017-03-16 19:17:26 -0400

    Made it object-oriented

--- directional_shooting.py
+++ directional_shooting.py
@@ -7,8 +7,8 @@ pygame.init()
 
 # Window
 SIZE = (800, 600)
-TITLE = "Bang!"
-screen = pygame.display.set_mode(SIZE)
+TITLE = "Pew! Pew! Pew!"
+window = pygame.display.set_mode(SIZE)
 pygame.display.set_caption(TITLE)
 
 # Timer
@@ -20,11 +20,42 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 
-# Game objects
-gun = {'x': 400, 'y': 300}
-bullets = []
-bullet_speed = 5
+# Game classes
+class Gun:
 
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def draw(self, surface):
+        loc = round(self.x), round(self.y) 
+        pygame.draw.circle(surface, WHITE, loc, 10)
+        
+class Bullet:
+
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
+
+    def update(self):
+        self.x += self.vx
+        self.y += self.vy
+
+    def draw(self, surface):
+        loc = round(self.x), round(self.y)
+        pygame.draw.circle(surface, RED, loc, 10)
+
+
+# Game objects
+gun =  Gun(400, 300)
+bullets = []
+
+# Game settings
+speed = 5
+
+# Helper functions
 def get_velocity_vector(x1, y1, x2, y2, speed):
     #return x2 - x1, y2 - y1
     #return (x2 - x1) / 10, (y2 - y1) / 10
@@ -47,35 +78,33 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
         elif event.type == pygame.MOUSEBUTTONUP:
-            x1, y1 = gun['x'], gun['y']
+            x1, y1 = gun.x, gun.y
             x2, y2 = pygame.mouse.get_pos()
-
-            vx, vy = get_velocity_vector(x1, y1, x2, y2, bullet_speed)
             
-            b = {'x': x1, 'y': y1, 'vx': vx, 'vy': vy}
+            vx, vy = get_velocity_vector(x1, y1, x2, y2, speed)
+            
+            b = Bullet(x1, y1, vx, vy)
             bullets.append(b)
 
     # Game logic
     to_remove = []
     
     for b in bullets:
-        b['x'] += b['vx']
-        b['y'] += b['vy']
+        b.update()
 
-        if b['x'] < 0 or b['x'] > 800 or b['y'] < 0 or b['y'] > 600:
+        if b.x < 0 or b.x > 800 or b.y < 0 or b.y > 600:
             to_remove.append(b)
 
     for b in to_remove:
         bullets.remove(b)
 
     # Drawing code
-    screen.fill(BLACK)
-    pygame.draw.circle(screen, WHITE, (gun['x'], gun['y']), 10)
-
+    window.fill(BLACK)
+    
+    gun.draw(window)
+    
     for b in bullets:
-        b_x = round(b['x'])
-        b_y = round(b['y'])
-        pygame.draw.circle(screen, RED, (b_x, b_y), 10)
+        b.draw(window)
         
     # Update screen (Actually draw the picture in the window.)
     pygame.display.flip()
